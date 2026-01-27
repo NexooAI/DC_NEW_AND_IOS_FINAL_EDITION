5951b2cd590b87799847976737b78f705bc06474
mohandass9788 <mohandass9788@gmail.com>
2026-01-28 02:32:37 +0530

    build: Increment app version to 3.0.2, Android versionCode to 7, and update Expo dependencies.

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     ...config,
     name: themeConfig.customerName,
     slug: themeConfig.slug,
-    version: "3.0.1",
+    version: "3.0.2",
     orientation: "portrait",
     userInterfaceStyle: "automatic",
     scheme: "acme",
@@ -28,7 +28,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     android: {
         package: themeConfig.bundleIdentifier,
         googleServicesFile: "./google-services.json",
-        versionCode: 6,
+        versionCode: 7,
 
         splash: {
             image: "./assets/images/playstore-icon.png",
