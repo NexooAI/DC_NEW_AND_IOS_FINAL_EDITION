586e0464730bd605a44051e195b22dbf870d0bf6
mohandass9788 <mohandass9788@gmail.com>
2026-06-26 09:53:32 +0530

    fix: switch ios configuration from static frameworks to modular headers in app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -154,7 +154,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
                         ],
                     },
                     ios: {
-                        useFrameworks: "static",
+                        useModularHeaders: true,
                     },
                 },
             ],
